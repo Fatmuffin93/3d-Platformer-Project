aeb996a2405f91c1955b76d7961793728b8f09b5
Fatmuffin93 <dbukhari807@gmail.com>
2016-10-16 12:46:31 -0400

    Beginning
    Basic movement and camera controller

---
+++ Banjo Ka-new-ie/Assets/CameraControl.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 myPos;
+	private Quaternion rotation = new Quaternion(0.4f,0f,0f,0.9f);
+	private bool rotationPositive = false;
+	private bool rotationNegative = false;
+	private int rotationCountdown = 45;
+	private bool firstPersonView = false;
+	private bool changeCamera = false;
+	private Vector3 mouse_pos;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!firstPersonView && !changeCamera) {
+			//third-person camera
+			rotation = transform.rotation;
+			if (rotationPositive && rotationCountdown > 0) {
+				myPos = RotateAroundPoint (myPos, Vector3.zero, Quaternion.Euler (0, 2f, 0));
+				transform.RotateAround (player.transform.position, Vector3.up, 2f);
+				rotationCountdown--;
+			}
+
+			if (rotationNegative && rotationCountdown > 0) {
+				myPos = RotateAroundPoint (myPos, Vector3.zero, Quaternion.Euler (0, -2f, 0));
+				transform.RotateAround (player.transform.position, Vector3.up, -2f);
+				rotationCountdown--;
+			}
+
+			if (rotationCountdown <= 0) {
+				rotationNegative = false;
+				rotationPositive = false;
+				rotationCountdown = 45;
+			}
+			transform.position = player.transform.position + myPos;
+
+			if (Input.GetKeyDown (KeyCode.E) && !rotationNegative) {
+				rotationPositive = true;
+			}
+
+			if (Input.GetKeyDown (KeyCode.Q) && !rotationPositive) {
+				rotationNegative = true;
+			}
+
+		} else if (firstPersonView && !changeCamera) {
+
+			//first-person camera
+			mouse_pos = Input.mousePosition;
+			mouse_pos.x -= Screen.width/2;
+			mouse_pos.y -= Screen.height/2;
+			/*var screenPoint = Camera.main.WorldToScreenPoint (transform.localPosition);
+			var offset = new Vector3 (mouse_pos.x - screenPoint.x, 0, mouse_pos.y - screenPoint.y);
+			Debug.Log (offset);
+			var angle = Mathf.Atan2 (offset.z, offset.x) * Mathf.Rad2Deg;*/
+
+			transform.rotation = Quaternion.Euler (-mouse_pos.y * Time.deltaTime * 5, mouse_pos.x * Time.deltaTime * 5, 0);
+            transform.position = player.transform.position;
+            /*Vector3 MousePosition = Input.mousePosition;
+			MousePosition.x = (Screen.height/2) - Input.mousePosition.y;
+			MousePosition.y = -(Screen.width/2) + Input.mousePosition.x;
+			transform.Rotate(MousePosition * Time.deltaTime * 0.2f, Space.Self);
+			transform.position = player.transform.position;*/
+
+        } else if (changeCamera && !firstPersonView) {
+			//change to first-person view
+			if(!player.GetComponent<Player>().firstPersonView){
+				player.GetComponent<Player>().firstPersonView = true;
+			}
+			if(transform.position != player.transform.position){
+				transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 20 * Time.deltaTime);
+				transform.rotation = rotation;
+			} else{
+				firstPersonView = true;
+				changeCamera = false;
+			}
+
+		} else {
+
+			//change to third-person view
+			if(transform.position != player.transform.position + myPos){
+				transform.position = Vector3.MoveTowards(transform.position, player.transform.position + myPos, 20 * Time.deltaTime);
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+			} else{
+				firstPersonView = false;
+				changeCamera = false;
+				player.GetComponent<Player>().firstPersonView = false;
+			}
+
+		}
+		if (Input.GetKeyDown (KeyCode.F) && !changeCamera) {
+			changeCamera = true;
+		}
+	}
+
+	Vector3 RotateAroundPoint(Vector3 point, Vector3 pivot, Quaternion angle){
+		return angle * ( point - pivot) + pivot;
+	}
+
+}
---
+++ Banjo Ka-new-ie/Assets/Player.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float speed = 5.0f;
+	private Vector3 mouse_pos;
+	public bool firstPersonView;
+    private GameObject tree;
+	
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!firstPersonView && tree == null) {
+			//rotate the player
+			mouse_pos = Input.mousePosition;
+			var screenPoint = Camera.main.WorldToScreenPoint (transform.localPosition);
+			var offset = new Vector3 (mouse_pos.x - screenPoint.x, 0, mouse_pos.y - screenPoint.y);
+			var angle = Mathf.Atan2 (offset.z, offset.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler (0, -angle + 90, 0);
+			
+			if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+				//move up
+				transform.Translate (Vector3.forward * Time.deltaTime * speed);
+			}
+			if (Input.GetKey (KeyCode.Space)) {
+				//move up
+				transform.GetComponent<Rigidbody>().velocity = new Vector3 (0, 7, 0);
+			}
+			/*if (transform.GetComponent<Rigidbody>().velocity.y >= 0) {
+				speed = 5f;
+			} else {
+				speed = 0.5f;
+			}*/
+		} else if (tree != null) {
+            if (Input.GetKey(KeyCode.D)){
+                transform.RotateAround(tree.transform.position, Vector3.up, -315f * Time.deltaTime);   
+            }
+            if (Input.GetKey(KeyCode.A)){
+                transform.RotateAround(tree.transform.position, Vector3.up, 315f * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.W)){
+                transform.Translate(Vector3.up * Time.deltaTime * 5f);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * -5f);
+            }
+            if (Input.GetKeyDown(KeyCode.Space)){
+                tree = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                transform.GetComponent<Rigidbody>().velocity = new Vector3(-5, 3, 0);
+            }
+        }
+        else {
+
+		}
+	}
+
+    void OnCollisionEnter(Collision col){
+        if(col.gameObject.tag == "Tree"){
+            GetComponent<Rigidbody>().useGravity = false;
+            tree = col.gameObject;
+            transform.LookAt(new Vector3(col.gameObject.transform.position.x, transform.position.y, col.gameObject.transform.position.z));
+        }
+    }
+
+    void OnCollisionExit(Collision col){
+        if (col.gameObject.tag == "Tree"){
+            tree = null;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
